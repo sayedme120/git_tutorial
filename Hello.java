caff205c3458514ac001e7d4a2a5ca011f6f23e0
sayedme120 <sayedme120@gmail.com>
2018-12-17 18:54:50 +0600

    Added a new method called gretting

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello{
-	public static void public static void main(String[] args) {
+	 public static void main(String[] args) {
 		System.out.println("Vs Code is Awesomr....");
+		gretting("ProSayed");
+	}
+
+	public static void gretting(String[] args) {
+		System.out.println("Welcome  " + name);
 	}
 }
